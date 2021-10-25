b54819f17b0517f2613c9501acca9e00c91c11a5
Rogerio Esteves <rogerio.esteves68@gmail.com>
2021-10-24 21:37:40 -0300

    video aula

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Listar todos os restaurantes
+  get 'restaurants', to: 'restaurants#index'
+
+# Criar form new
+  get 'restaurants/new', to: 'restaurants#new'
+
+  # Listar 1 restaurante (pagina de show)
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # Criar novo restaurant
+  post 'restaurants', to: 'restaurants#create'
+
+  # Criar form edit
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+
+  # Update de restaurant
+  patch 'restaurants/:id', to: 'restaurants#update'
+
+  # Delete
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
 end
